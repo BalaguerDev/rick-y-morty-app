b36831a227b931ff96b4418599ba275430d39e77
abalaguer-webimpacto <abalaguer@webimpacto.es>
2023-10-18 15:51:27 +0200

    feat: :sparkles: Estilos textos tarjetas de personajes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import Logo from '../../assets/img/Rick_and_Morty.svg.png'
+import React, { useState, useEffect } from 'react';
+import Logo from '../../assets/img/Rick_and_Morty.svg.png';
 import SearchBar from '../SearchBar/SearchBar';
-import '../../styles/header.css'
+import '../../styles/header.css';
 
 const Header = ({ handleSearch }) => {
+  const [isSticky, setIsSticky] = useState(false);
+  const [isShrunk, setIsShrunk] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 0) {
+        setIsSticky(true);
+        setIsShrunk(true);
+      } else {
+        setIsSticky(false);
+        setIsShrunk(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="header">
-        <img src={Logo} alt="Logo Rick y Morty" />
-        <SearchBar handleSearch={handleSearch} className="searchBar" />
+    <div className={`header ${isSticky ? 'sticky' : ''}`}>
+      <img
+        src={Logo}
+        alt="Logo Rick y Morty"
+        className={isShrunk ? 'shrunk' : ''} // Agrega una clase cuando la imagen se ha encogido
+      />
+      <SearchBar handleSearch={handleSearch} className="searchBar" />
     </div>
   );
 };
